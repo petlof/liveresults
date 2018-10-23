8d456b13e00fca334d41c3c80f4568423768aee1
Peter Löfås <peter.lofas@triona.se>
2018-10-23 08:54:26 +0200

    Removed svnbridge junk

--- tags/web-after-eoc2012/sipunch.php
+++
@@ -1,8 +0,0 @@
-<?
-$link = mysql_connect("localhost","root","vol8090ant");
-mysql_select_db("EMMA3",$link);
-
-mysql_query("insert into gprspunches(si,code,received) values(".$_GET['sinumber'].",".$_GET['code'].",now())");
-echo("OK");
-mysql_close($link);
-?>
